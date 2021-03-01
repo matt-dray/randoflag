ae512e0e38b36e6227a08064d65c742c07d8bb3d
Matt Dray <mwdray@gmail.com>
2021-03-01 21:54:27 +0000

    Add readme, simplify repo structure, expand gitignore

---
+++ README.md
@@ -0,0 +1,22 @@
+
+# randoflag `r emo::ji("flags")`
+
+<!-- badges: start -->
+[![Project Status: Concept – Minimal or no implementation has been done yet, or the repository is only intended to be a limited example, demo, or proof-of-concept.](https://www.repostatus.org/badges/latest/concept.svg)](https://www.repostatus.org/#concept)
+<!-- badges: end -->
+
+Get vexed! A very simple Shiny app that presents a random (emoji) flag. Click to reveal the nation it represents.
+
+Basically, I wanted something interesting to open with each new browser tab on my phone.
+
+The app is live online at [shinyapps.io/mattdray/randoflag](https://shinyapps.io/mattdray/randoflag), until I decide to take it down. 
+
+You can always run the app from an R session with:
+
+``` r
+shiny::runGitHub(
+  repo = "randoflag", 
+  username = "matt-dray",
+  ref = "main"
+)
+```
---
+++ app.R
@@ -0,0 +1,61 @@
+library(shiny)
+library(shinyjs)
+library(bslib)
+library(emo)
+
+flags <- emo::jis[emo::jis$subgroup == "country-flag", c("emoji", "name")]
+
+ui <- fluidPage(
+  
+  theme = bs_theme(
+    bg = "#333333",
+    fg = "#FFFFFF",
+    primary = "#BEBEBE",
+    base_font = font_google("Varela Round")
+  ),
+  
+  shinyjs::useShinyjs(),
+  
+  tags$head(tags$style(
+    "* { text-align: center; }",
+    "#flagEmoji { font-size: 200px }",
+    "#flagName { font-size: 25px }",
+    "footer { position: absolute; bottom:5%; left: 5%; right:5% }"
+  )),
+  
+  textOutput("flagEmoji"),
+  actionButton("reveal_btn", paste("Reveal", emo::ji("check"))),
+  br(),
+  br(),
+  hidden(div(id = "reveal_div", textOutput("flagName"))),
+  br(),
+  
+  actionButton(
+    "refresh_btn",
+    paste("Refresh", emo::ji("arrows_counterclockwise"))
+  ),
+  
+  HTML(
+    "<footer>
+          <a href='https://www.matt-dray.com/'>Matt Dray</a>
+          </footer>"
+  )
+  
+)
+
+server <- function(input, output) {
+  
+  flag <- flags[sample(nrow(flags), 1), ]
+  
+  output$flagEmoji <- renderText({ flag[["emoji"]] })
+  
+  observeEvent(input$refresh_btn, { shinyjs::refresh() })
+  
+  observeEvent(input$reveal_btn, {
+    toggle('reveal_div')
+    output$flagName <- renderText({ flag[["name"]] })
+  })
+  
+}
+
+shinyApp(ui = ui, server = server)
